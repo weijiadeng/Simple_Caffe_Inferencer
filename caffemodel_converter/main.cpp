b91c69dae9f8e4528bad2f6a91916ab7d610b4c2
pcgsf22 <pcgsf22@gmail.com>
2018-09-30 18:49:44 +0800

    Add the function of specify output precision. Add the support of V1 version caffemodel. Mend the bug that layer name with illegal linux filename characters would break the program.

--- caffemodel_converter/main.cpp
+++ caffemodel_converter/main.cpp
@@ -9,6 +9,11 @@
 #include "main.h"
 #include <cstring>
 #include <sstream>
+#include "external/half.h"
+#include <regex>
+#include <algorithm>
+
+
 
 using std::string;
 using std::ifstream;
@@ -38,19 +43,36 @@ std::string num_to_string(int num) {
     return oss.str();
 }
 
-
-bool get_parameters_form_caffemodel(string const &model_path, string const &out_path) {
-    ifstream ifs(model_path, ifstream::in | ifstream::binary);
-    if ((&ifs == nullptr) ||(!ifs.is_open())) {
-        return false;
+int judge_version(caffe::NetParameter const & net){
+    if(net.layer().size()!=0)
+        return 2;
+    else if (net.layers().size()!=0)
+    {
+        if(net.layers(0).has_layer())
+            return 0;
+        else
+            return 1;
     }
-    caffe::NetParameter netparam;
-    bool ok = netparam.ParseFromIstream(&ifs);
+}
+
+string name_reconstruct(const string & str){
+
+    string pattern{R"lit([\*?/\\\[\]\{\}\(\)"'`<>;!&])lit"};
+    std::regex re(pattern);
+
+    string res{"-"};
+
+    return std::regex_replace(str,re,res);
+
+}
+
+template <typename T>
+bool get_parameters_v2(const string & out_path, caffe::NetParameter const & netparam ){
     for (size_t i = 0; i < netparam.layer_size(); i++) {
         if(netparam.layer(i).blobs().empty())
             continue;
         string filename;
-        filename = out_path+netparam.layer(i).name()+".txt";
+        filename = out_path+name_reconstruct(netparam.layer(i).name())+".txt";
         printf("Processing %s!\n",filename.c_str());
         std::ofstream ofs(filename.c_str());
         int num = netparam.layer(i).blobs().size();
@@ -59,8 +81,8 @@ bool get_parameters_form_caffemodel(string const &model_path, string const &out_
             auto size = netparam.layer(i).blobs(j).shape().dim().size();
             if(size<SUPPOSED_DIM)
                 for(int i=0;i<SUPPOSED_DIM-size;i++){
-                ofs<<"1 ";
-            }
+                    ofs<<"1 ";
+                }
             for(int k = 0;k<size-1;k++) {
                 ofs<<netparam.layer(i).blobs(j).shape().dim(k)<<" ";
             }
@@ -69,10 +91,11 @@ bool get_parameters_form_caffemodel(string const &model_path, string const &out_
             auto width = netparam.layer(i).blobs(j).shape().dim(size-1);
             auto width_count= static_cast<int>(1 % width);
             for(auto & x:netparam.layer(i).blobs(j).data()) {
+                ofs<<T(x);
                 if(width_count)
-                    ofs<<x<<" ";
+                    ofs<<" ";
                 else
-                    ofs<<x<<endl;
+                    ofs<<endl;
                 width_count+=1;
                 width_count%=width;
             }
@@ -82,6 +105,77 @@ bool get_parameters_form_caffemodel(string const &model_path, string const &out_
             }
         }
     }
+    return true;
+
+}
+
+template <typename T>
+bool get_parameters_v1(const string & out_path, caffe::NetParameter const & netparam ){
+    for (size_t i = 0; i < netparam.layers_size(); i++) {
+        int k =netparam.layers().size();
+        if(netparam.layers(i).blobs().empty())
+            continue;
+        string filename;
+        filename = out_path+name_reconstruct(netparam.layers(i).name())+".txt";
+        printf("Processing %s!\n",filename.c_str());
+        std::ofstream ofs(filename.c_str());
+        int num = netparam.layers(i).blobs().size();
+        ofs<<num<<endl;
+        for(int j=0;j<num;j++) {
+            ofs<<netparam.layers(i).blobs(j).num()<<" "<<netparam.layers(i).blobs(j).channels()
+               <<" "<<netparam.layers(i).blobs(j).height()<<" "<<netparam.layers(i).blobs(j).width()<<endl;
+            auto width = netparam.layers(i).blobs(j).width();
+            auto width_count= static_cast<int>(1 % width);
+            for(auto & x:netparam.layers(i).blobs(j).data()) {
+                ofs<<T(x);
+                if(width_count)
+                    ofs<<" ";
+                else
+                    ofs<<endl;
+                width_count+=1;
+                width_count%=width;
+            }
+            if(!ofs)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+template <typename T>
+bool get_parameters_v0(const string & out_path, caffe::NetParameter const & netparam ){
+   printf("The v0 version caffemodel file is not supported yet!\n");
+   return false;
+
+}
+
+template <typename T>
+bool get_parameters_form_caffemodel(string const &model_path, string const &out_path) {
+    ifstream ifs(model_path, ifstream::in | ifstream::binary);
+    if ((&ifs == nullptr) ||(!ifs.is_open())) {
+        return false;
+    }
+    caffe::NetParameter netparam;
+    bool ok = netparam.ParseFromIstream(&ifs);
+
+    switch (judge_version(netparam)){
+        case 0:
+            ok = ok& get_parameters_v1<T>(out_path,netparam);
+            break;
+        case 1:
+            ok = ok& get_parameters_v1<T>(out_path,netparam);
+            break;
+        case 2:
+            ok = ok& get_parameters_v2<T>(out_path,netparam);
+            break;
+        default:
+            ok = false;
+            printf("Unsupported caffemodel version!\n");
+    }
+
     ifs.close();
     google::protobuf::ShutdownProtobufLibrary();
     return true;
@@ -129,6 +223,20 @@ int main(int argc,char *argv[]) {
 
             testAppArgs.output_folder = std::string(argv[++ii]);
         }
+
+        else if (std::strcmp(arg, "--precision") == 0) // output model folder path
+        {
+            if (ii + 1 >= argc) {
+                // Expecting another parameter
+                testAppArgs.show_help = true;
+                break;
+            }
+
+            testAppArgs.precision = std::string(argv[++ii]);
+            //to lower case
+            std::transform(testAppArgs.precision.begin(),testAppArgs.precision.end(),testAppArgs.precision.begin(),::tolower);
+        }
+
         else // unknown
         {
             testAppArgs.show_help = true;
@@ -146,6 +254,20 @@ int main(int argc,char *argv[]) {
         testAppArgs.output_folder += "output";
     }
 
+    if(!strcmp(testAppArgs.precision.c_str(), "fp16")){
+        testAppArgs.precision_code = FP16;
+    }
+    else if(!strcmp(testAppArgs.precision.c_str(), "fp32")){
+        testAppArgs.precision_code = FP32;
+    }
+    else if(!strcmp(testAppArgs.precision.c_str(), "fp64")){
+        testAppArgs.precision_code = FP64;
+    }
+    else{
+        testAppArgs.show_help = true;
+    }
+
+
 
     if (testAppArgs.show_help) {
         printf("Usage: %s [-options] --input <caffemodel_path> --output <output_path>\n", argv[0]);
@@ -153,6 +275,8 @@ int main(int argc,char *argv[]) {
         printf("    -h                            print this help message\n");
         printf("    --input <caffemodel_path>     caffemodel file path\n");
         printf("    --output <output_file>        output folder path\n");
+        printf("    --precision <data_type>       output data precision, currently\n"
+               "                                  support fp16, fp32 and fp64\n");
         return 0;
 
     }
@@ -177,7 +301,17 @@ int main(int argc,char *argv[]) {
 
     folder_name += "/";
 
-    if (!get_parameters_form_caffemodel(testAppArgs.input_name, folder_name)) {
+    bool ok = true;
+
+    switch (testAppArgs.precision_code){
+        case FP16: ok = get_parameters_form_caffemodel<half_float::half>(testAppArgs.input_name, folder_name);
+            break;
+        case FP32: ok = get_parameters_form_caffemodel<float>(testAppArgs.input_name, folder_name);
+            break;
+        case FP64: ok = get_parameters_form_caffemodel<double>(testAppArgs.input_name, folder_name);
+            break;
+    }
+    if (!ok) {
         printf("Error when writing files!\n");
         return 0;
     }
@@ -187,3 +321,4 @@ int main(int argc,char *argv[]) {
     return 0;
 
 }
+
--- caffemodel_converter/main.h
+++ caffemodel_converter/main.h
@@ -8,18 +8,24 @@
 #include <string>
 #include <vector>
 
-
+#define FP16 0
+#define FP32 1
+#define FP64 2
 
 
 struct TestAppArgs
 {
     std::string input_name;
     std::string output_folder;
+    std::string precision;
+    int precision_code;
     bool show_help;
     TestAppArgs() :
             input_name(""),
-            output_folder(""),
-            show_help(false)
+            output_folder("./"),
+            show_help(false),
+            precision("fp32"),
+            precision_code(0)
     {}
 };
 
@@ -33,3 +39,4 @@ std::string num_to_string(int num);
 
 
 #endif //MODEL_CONVERTER_MAIN_H
+
